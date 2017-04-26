52e07bd40355eb6a4de3d32ab8c19f35dcabe9ed
Jimmy Campbell <jimmyca@microsoft.com>
2017-04-26 14:46:14 -0700

    Fix webserver info's supports_sni property on server 2012.

--- src/Microsoft.IIS.Administration.WebServer.Info/InfoHelper.cs
+++ src/Microsoft.IIS.Administration.WebServer.Info/InfoHelper.cs
@@ -52,7 +52,7 @@ namespace Microsoft.IIS.Administration.WebServer.Info
             var obj = new {
                 name = "Microsoft Internet Information Services",
                 id = WebServerId.CreateFromPath(ManagementUnit.Current.ApplicationHostConfigPath).Uuid,
-                supports_sni = version != null && version > new Version(8, 0),
+                supports_sni = version != null && version >= new Version(8, 0),
                 status = Enum.GetName(typeof(Status), GetStatus()).ToLower(),
                 version = GetVersion()
             };
