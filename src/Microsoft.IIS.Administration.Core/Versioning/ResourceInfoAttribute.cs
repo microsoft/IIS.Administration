de6252c1f7f4abd6f609fbb6917ead5101e3bcd9
babula38 <babula38@gmail.com>
2017-11-13 20:41:27 +0530

    Revert "Fix for application is repeated in content types #169"
    This reverts commit 14c8a601eb52b83dc92876a07dee374e46f43e0d.

--- src/Microsoft.IIS.Administration.Core/Versioning/ResourceInfoAttribute.cs
+++ src/Microsoft.IIS.Administration.Core/Versioning/ResourceInfoAttribute.cs
@@ -1,4 +1,3 @@
-
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
@@ -46,8 +45,6 @@ namespace Microsoft.IIS.Administration.Core
             }
             else {
                 type.Append(".");
-                if (previous.Contains("application/hal"))
-                    previous = previous.Remove(0, "application/".Length);
                 type.Append(previous);
             }
 
