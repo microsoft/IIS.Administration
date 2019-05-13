8a4f4d0905f273099af1360d7728a018a916aa3c
Peter Hsu <shhsu@microsoft.com>
2019-05-13 15:20:24 -0700

    vsts build fix

---
+++ azure-pipelines/powershell/Copy-SignedBits.ps1
@@ -0,0 +1,41 @@
+param(
+    [string]
+    $buildDir,
+
+    [string]
+    $publishDir
+)
+
+$ErrorActionPreference = "Stop"
+
+if (!$buildDir -or !$publishDir) {
+    try {
+        $projectRoot = git rev-parse --show-toplevel
+        Write-Information "Detected project root ${projectRoot} with git"
+    } catch {
+        $projectRoot = [System.IO.Path]::Combine($PSScriptRoot, "..", "..")
+        Write-Information "Detected project root ${projectRoot} from script location"
+    }
+
+    if (!$buildDir) {
+        $buildDir = Join-Path $projectRoot ".build"
+    }
+
+    if (!$publishDir) {
+        $publishDir = Join-Path $projectRoot "dist"
+    }
+}
+
+Push-Location $buildDir
+try {
+    foreach ($bitPath in Get-ChildItem -Recurse -Filter *.dll | Resolve-Path -Relative) {
+        $builtBit = Join-Path $buildDir $bitPath
+        $publishedBit =  Join-Path $publishDir $bitPath
+        if (!(Test-Path $publishedBit)) {
+            Write-Error "Cannot find published bit $publishedBit"
+        }
+        Copy-Item -Path $builtBit -Destination $publishedBit -Force
+    }
+} finally {
+    Pop-Location
+}
