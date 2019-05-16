0226646813f7c8ea443a802b4273ecf867fc9e79
Peter Hsu <shhsu@microsoft.com>
2019-05-16 13:39:15 -0700

    logging

--- azure-pipelines/scripts/Copy-SignedBits.ps1
+++ azure-pipelines/scripts/Copy-SignedBits.ps1
@@ -27,13 +27,16 @@ if (!$buildDir -or !$publishDir) {
 }
 
 Push-Location $buildDir
+Write-Verbose "Locate build directory $buildDir"
 try {
     foreach ($bitPath in Get-ChildItem -Recurse -Filter *.dll | Resolve-Path -Relative) {
+        Write-Verbose "Locate built bit $bitPath"
         $builtBit = Join-Path $buildDir $bitPath
         $publishedBit =  Join-Path $publishDir $bitPath
         if (!(Test-Path $publishedBit)) {
             Write-Error "Cannot find published bit $publishedBit"
         }
+        Write-Verbose "Copied built bit to $publishedBit"
         Copy-Item -Path $builtBit -Destination $publishedBit -Force
     }
 } finally {
